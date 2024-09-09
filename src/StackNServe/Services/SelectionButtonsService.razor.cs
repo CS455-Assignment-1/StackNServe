b465cbab50022ea933cbaf7bc5fe60d8c02820c6
AditiKhandelia <khandeliaaditi@gmail.com>
2024-09-09 15:41:08 +0530

    Fix Selection Buttons, Fixes #15

---
+++ src/StackNServe/Services/SelectionButtonsService.razor.cs
@@ -0,0 +1,11 @@
+public class SelectionButtonService
+{
+    public bool BunSelectVar { get; set; }
+    public bool PattySelectVar { get; set; }
+    public bool SaucesSelectVar { get; set; }
+    public bool ToppingSelectVar { get; set; }
+
+    public event Action OnChange;
+
+    public void NotifyStateChanged() => OnChange?.Invoke();
+}
