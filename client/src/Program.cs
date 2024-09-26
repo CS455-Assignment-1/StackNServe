4b8c2688f9d24333d3a88bcc111a86096db18715
AditiKhandelia <khandeliaaditi@gmail.com>
2024-09-26 17:55:13 +0530

    Establish communication between server and client, Fixes #31

--- client/src/Program.cs
+++ client/src/Program.cs
@@ -1,20 +1,25 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using StackNServe.Services;
 using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+namespace StackNServe
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
 
-using StackNServe;
-using StackNServe.Services;
+            // Register HttpClient
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<GlobalStringListService>();
+            builder.Services.AddScoped<SelectionButtonService>();
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton<GlobalStringListService>();
-
-// Replacing the misplaced end-of-line marker with two newlines
-builder.Services.AddScoped<SelectionButtonService>();
-
-await builder.Build().RunAsync();
+            await builder.Build().RunAsync();
+        }
+    }
+}
--- server/server.py
+++ server/server.py
@@ -1,27 +1,31 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    
-    def _set_headers(self):
+    def do_GET(self):
+        if self.path == "/initialBalance":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.send_header('Access-Control-Allow-Origin', '*')  # Allow requests from any origin
+            self.end_headers()
+            self.wfile.write(b"150")  # Returning initial balance as 150
+        else:
+            self.send_response(400)
+            self.end_headers()
+
+    # Allowing CORS for preflight requests
+    def do_OPTIONS(self):
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.send_header('Access-Control-Allow-Origin', '*')  # Allow Blazor client to connect
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
         self.end_headers()
 
-    # Handle GET requests
-    def do_GET(self):
-        self._set_headers()
-        message = {"message": "Server is running and connected!"}
-        self.wfile.write(json.dumps(message).encode('utf-8'))
-
-    # Handle POST requests (for testing POST request connectivity)
-    def do_POST(self):
-        self._set_headers()
-        response = {"message": "POST request received and connected!"}
-        self.wfile.write(json.dumps(response).encode('utf-8'))
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Server running on port {port}...')
+    httpd.serve_forever()
 
 if __name__ == "__main__":
-    httpd = HTTPServer(('', 8000), SimpleHTTPRequestHandler)
-    print("Python server running on port 8000...")
-    httpd.serve_forever()
+    run()
+
