8272571610c5e1446974fe7cee3547963b43621b
AditiKhandelia <khandeliaaditi@gmail.com>
2024-10-05 18:23:44 +0530

    Establish connection between MongoDB database and server, Fixes #32

--- server/server.py
+++ server/server.py
@@ -1,18 +1,41 @@
+#!python
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import sys
+sys.path.insert(0, "/opt/homebrew/lib/python3.11/site-packages")
+from pymongo import MongoClient
+
+# Add the MongoDB connection string, and create collections
+client = MongoClient("mongodb+srv://StackNServe:aditi_kushagra@cluster0.rvwjh.mongodb.net/")
+
+BurgerDB = client["burger"]
+BunBurgerColection = BurgerDB["bun"]
+PattyBurgerColection = BurgerDB["patty"]
+ToppingsBurgerColection = BurgerDB["toppings"]
+SaucesBurgerColection = BurgerDB["sauces"]
+
+# Check connection
+print(client.list_database_names())
+print(BurgerDB.list_collection_names())
+chicken_patty_price = PattyBurgerColection.find_one({"Name": "Chicken Patty"})["Price"]
+print(chicken_patty_price)
+num_sauces = SaucesBurgerColection.count_documents({})
+print(num_sauces)
+jalapenos_description = ToppingsBurgerColection.find_one({"Name": "Jalapenos"})["Description"]
+print(jalapenos_description)
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/initialBalance":
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
-            self.send_header('Access-Control-Allow-Origin', '*')  # Allow requests from any origin
+            self.send_header('Access-Control-Allow-Origin', '*')  
             self.end_headers()
-            self.wfile.write(b"150")  # Returning initial balance as 150
+            self.wfile.write(b"150")  
         else:
             self.send_response(400)
             self.end_headers()
 
-    # Allowing CORS for preflight requests
     def do_OPTIONS(self):
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
