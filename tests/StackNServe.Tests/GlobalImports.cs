e8bb7959152b7f45a6ce6c4b45b00c5ad23447ff
AditiKhandelia <khandeliaaditi@gmail.com>
2024-10-09 08:21:47 +0530

    Add unit tests for client/src/Pages, See #37

--- tests/StackNServe.Tests/GlobalImports.cs
+++ tests/StackNServe.Tests/GlobalImports.cs
@@ -3,3 +3,4 @@ global using StackNServe;
 global using StackNServe.Services;
 global using StackNServe.Pages;
 global using StackNServe.Shared;
+global using StackNServe.Services;
--- tests/StackNServe.Tests/Pages.Tests/Player_Guide.Tests.cs
+++ tests/StackNServe.Tests/Pages.Tests/Player_Guide.Tests.cs
@@ -1,57 +1,69 @@
 using Bunit;
 using Xunit;
 using StackNServe.Pages;
+using StackNServe.Services;
+using StackNServe.Shared;
+using Microsoft.Extensions.DependencyInjection;
+using System.Security.Cryptography;
+using Microsoft.JSInterop;
+using Moq;
+using Moq.Protected;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Threading;
 
-namespace StackNServe.Tests;
-public class PlayerGuideTests : TestContext
+namespace StackNServe.Tests
 {
-    [Fact]
-    public void PlayerGuide_Heading_Should_Render_Correctly()
+    public class PlayerGuideTests : TestContext
     {
-        // Arrange & Act
-        var component = RenderComponent<Player_Guide>(); 
+        [Fact]
+        public void PlayerGuidePage_PageRenders()
+        {
+            var component = RenderComponent<Player_Guide>();
+            
+            var heading = component.Find("h1.Player_Guide_Heading");
+            var greeting = component.Find("p.Player_Guide_Greeting");
+            var rules = component.Find("div.Player_Guide_Rules");
+            var intent = component.Find("p.Player_Guide_Intent");
+            var playAgainButton = component.Find("div.Play_Game_Button");
 
-        // Assert
-        var heading = component.Find(".Player_Guide_Heading");
-        Assert.Equal("Player Guide", heading.TextContent);
-    }
+            Assert.NotNull(heading);
+            Assert.NotNull(greeting);
+            Assert.NotNull(rules);
+            Assert.NotNull(intent);
+            Assert.NotNull(playAgainButton);
+        }
 
-    [Fact]
-    public void PlayerGuide_Rules_Should_Render_Seven_Rules()
-    {
-        // Arrange & Act
-        var component = RenderComponent<Player_Guide>();
+        public void PlayerGuidePage_HeadingContentIsCorrect()
+        {
+            var component = RenderComponent<Player_Guide>();
+            var heading = component.Find("h1.Player_Guide_Heading");
 
-        // Assert
-        var rules = component.Find(".Player_Guide_Rules");
-        Assert.Contains("Customers will come to your restaurant and order a burger.", rules.TextContent);
-        Assert.Contains("Your initial balance in the game is Rs. 100.", rules.TextContent);
-        Assert.Contains("The time allotted to make the burgers is 120 seconds.", rules.TextContent);
+            Assert.Equal("Player Guide", heading.TextContent);
+        }
 
-        var listItems = component.FindAll("li");
-        Assert.Equal(7, listItems.Count); 
-    }
+        [Fact]
+        public void PlayerGuidePage_RulesContentIsCorrect()
+        {
+            var component = RenderComponent<Player_Guide>();
 
-    [Fact]
-    public void PlayerGuide_Intent_Should_Render_Correctly()
-    {
-        // Arrange & Act
-        var component = RenderComponent<Player_Guide>();
+            var rules = component.Find("div.Player_Guide_Rules");
+            var ruleItems = component.FindAll("li");
 
-        // Assert
-        var intentParagraph = component.Find(".Player_Guide_Intent");
-        Assert.Contains("The intent of the game is to test your memory and speed.", intentParagraph.TextContent);
-        Assert.Contains("You need to calculate if the current order is profitable or not.", intentParagraph.TextContent);
-    }
+            Assert.Equal(7, ruleItems.Count); 
+        }
 
-    [Fact]
-    public void Play_Again_Button_Should_Render_Correctly()
-    {
-        // Arrange & Act
-        var component = RenderComponent<Player_Guide>();
+        [Fact]
+        public void PlayerGuidePage_IntentContentIsCorrect()
+        {
+            var component = RenderComponent<Player_Guide>();
 
-        // Assert
-        var playAgainButton = component.FindComponent<Play_Again>();
-        Assert.NotNull(playAgainButton); 
-    }
+            var intent = component.Find("p.Player_Guide_Intent");
+            Assert.Equal("The intent of the game is to test your memory and speed. \n        You need to remember where to find the ingredients asked for, and their prices.\n        You also need to be quick in making the burgers and serving them to the customers.\n        You need to calculate if the current order is profitable or not.\n        There is no option to remove any wrong ingredients you may have stacked. So, be careful while stacking the ingredients.", intent.TextContent.Trim());
+        }
+
+    }   
 }
