6341f739e834b20cb80208973c9e8e2520840904
AditiKhandelia <khandeliaaditi@gmail.com>
2024-11-09 10:02:23 +0530

    modified performance tests for client/Pages, see #41

--- performance tests/client/pages/Leaderboard.Tests.py
+++ performance tests/client/pages/Leaderboard.Tests.py
@@ -4,37 +4,35 @@ import unittest
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import random
 
 class TestLeaderboard(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Chrome()
+        cls.driver.maximize_window()
+        cls.driver.get("https://cs455-assignment-1.github.io/StackNServe/Leaderboard")
+        time.sleep(3)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
 
     def test_leaderboard_page_load(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get("https://cs455-assignment-1.github.io/StackNServe/Leaderboard")
-        time.sleep(3)
         self.assertIn("StackNServe", self.driver.title)
         print("Leaderboard Page Loaded")
-        self.driver.quit()
-    
+
     def test_leaderboard_page_loadtime(self):
-        self.driver = webdriver.Chrome()
         start = time.time()
         self.driver.get("https://cs455-assignment-1.github.io/StackNServe/Leaderboard")
-        self.driver.quit()
         end = time.time()
-        self.assertLess(end - start, 2)
-        print("Page load time is less than 2 seconds")
+        self.assertLess(end - start, 1, "Page load time exceeds 1 second")
+        print("Page load time is less than 1 second")
 
     def test_leaderboard_page_component_loadtime(self):
-        self.driver = webdriver.Chrome()
         start = time.time()
         self.driver.get("https://cs455-assignment-1.github.io/StackNServe/Leaderboard")
-        time.sleep(5)
+        time.sleep(3)
         self.driver.find_element(By.CLASS_NAME, "Leaderboard_Page")
         self.driver.find_element(By.CLASS_NAME, "Leaderboard_Heading")
         self.driver.find_element(By.CLASS_NAME, "Leaderboard_Table")
@@ -42,8 +40,8 @@ class TestLeaderboard(unittest.TestCase):
         self.driver.find_element(By.CLASS_NAME, "Leaderboard_Table_Header_Score")
         self.driver.find_element(By.CLASS_NAME, "Leaderboard_Table_Data")
         end = time.time()
-        self.assertLess(end - start, 6)
-        print("Component load time is less than 6 seconds")
+        self.assertLess(end - start, 4, "Component load time exceeds 6 seconds")
+        print("Component load time is less than 4 seconds")
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
--- performance tests/client/pages/Player_Guide.Tests.py
+++ performance tests/client/pages/Player_Guide.Tests.py
@@ -4,59 +4,54 @@ import unittest
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import random
 
 class TestPlayerGuide(unittest.TestCase):
-    # check if the page is loaded
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Chrome()
+        cls.driver.maximize_window()
+        cls.driver.get("https://cs455-assignment-1.github.io/StackNServe/")
+        time.sleep(3)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
 
     def test_player_guide_page_load(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get("https://cs455-assignment-1.github.io/StackNServe/")
-        time.sleep(3)
         self.assertIn("StackNServe", self.driver.title)
         print("Player Guide Page Loaded")
-        self.driver.quit()
 
     def test_player_guide_page_loadtime(self):
-        self.driver = webdriver.Chrome()
         start = time.time()
         self.driver.get("https://cs455-assignment-1.github.io/StackNServe/")
-        self.driver.quit()
         end = time.time()
-        self.assertLess(end - start, 2)
-        print("Page load time is less than 2 seconds")
+        self.assertLess(end - start, 1, "Page load time exceeds 2 seconds")
+        print("Page load time is less than 1 seconds")
 
     def test_player_guide_page_component_loadtime(self):
-        self.driver = webdriver.Chrome()
         start = time.time()
         self.driver.get("https://cs455-assignment-1.github.io/StackNServe/")
-        time.sleep(5)
+        time.sleep(3)
         self.driver.find_element(By.CLASS_NAME, "Player_Guide")
         self.driver.find_element(By.CLASS_NAME, "Player_Guide_Heading")
         self.driver.find_element(By.CLASS_NAME, "Player_Guide_Rules")
         self.driver.find_element(By.CLASS_NAME, "Player_Guide_Intent")
         self.driver.find_element(By.CLASS_NAME, "Play_Game_Button")
         end = time.time()
-        self.assertLess(end - start, 6)
-        print("Component load time is less than 6 seconds")
+        self.assertLess(end - start, 4, "Component load time exceeds 6 seconds")
+        print("Component load time is less than 4 seconds")
 
     def test_player_guide_page_button_click(self):
-        self.driver = webdriver.Chrome()
         self.driver.get("https://cs455-assignment-1.github.io/StackNServe/")
-        time.sleep(15)
+        time.sleep(3)
         self.driver.find_element(By.CLASS_NAME, "Play_Again").click()
-        time.sleep(5)
-        self.assertEqual("https://cs455-assignment-1.github.io/StackNServe/New_Game", self.driver.current_url)
+        time.sleep(3)
+        self.assertEqual("https://cs455-assignment-1.github.io/StackNServe/New_Game", self.driver.current_url, "Redirection URL mismatch after button click")
         self.driver.find_element(By.CLASS_NAME, "Player_Name_Input")
         self.driver.find_element(By.CLASS_NAME, "Player_Name_Label")
         self.driver.find_element(By.CLASS_NAME, "Player_Name_Field")
         self.driver.find_element(By.CLASS_NAME, "Player_Name_Button")
         print("Button click is working")
-        self.driver.quit()
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
